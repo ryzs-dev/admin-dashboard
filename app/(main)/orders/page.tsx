805f389379d72a87d2bb179387bd9689c2afc1bf
Ryan <rzshen0417@gmail.com>
2025-12-23 04:49:17 +0800

    chore(misc): remove unused files

--- app/(main)/orders/page.tsx
+++ app/(main)/orders/page.tsx
@@ -1,14 +1,23 @@
 'use client';
 
+import { Suspense } from 'react';
 import { OrderTable } from '@/components/modules/order/OrderTable';
 import { useOrders } from '@/hooks/useOrders';
 
-export default function OrdersPage() {
+function OrdersPageContent() {
   const { orders, isLoading } = useOrders();
 
   return (
     <div className="p-6">
       <OrderTable data={orders ?? []} isLoading={isLoading} />
     </div>
+  );
+}
+
+export default function OrdersPage() {
+  return (
+    <Suspense fallback={<div className="p-6">Loading orders...</div>}>
+      <OrdersPageContent />
+    </Suspense>
   );
 }
